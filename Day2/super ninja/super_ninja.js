e2bed70f9aab87f61c765fe59aa7e9dad69c2e29
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-12-06 01:34:43 +0300

    sixth assignment

---
+++ Day2/super ninja/super_ninja.js
@@ -0,0 +1,38 @@
+class Ninja {
+    constructor(name){
+        this.name= name;
+        this.health= 0;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName(){
+        console.log(this.name)
+    }
+
+    showStats = () => console.log("Name: "+this.name,"speed: "+this.speed, "strength "+this.strength, "health: "+this.health);
+
+    drinkSake = () => console.log(this.health +=10);
+}
+
+// const ninja1 = new Ninja("Hyabusa");
+// ninja1.sayName();
+// ninja1.showStats();
+// ninja1.drinkSake();
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name, 200, 10, 10)
+        this.wisdom = 10;
+    }
+        //  speakWisdom =() => {super.drinkSake()};
+        speakWisdom() {
+            // var x = super.drinkSake();
+            return this.drinkSake();
+        }
+}
+
+const superSensei = new Sensei("Hasan Sadaqa");
+superSensei.speakWisdom();
+superSensei.showStats();
+superSensei.wisdom;
