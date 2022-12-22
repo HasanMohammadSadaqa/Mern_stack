ce118a5b1cf1d003b5414b241c2fdde03a129c39
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-12-22 14:08:38 +0200

    editing the method used in axios

--- MongoDB/Jokes/server/routes/joke.routes.js
+++ MongoDB/Jokes/server/routes/joke.routes.js
@@ -5,7 +5,7 @@ module.exports = app =>{
     app.get("/api/jokes", JokeController.findAllJokes);
     app.get("/api/jokes/:_id", JokeController.findSpecificJoke);
     app.get("/api/jokes/random",JokeController.findSpecificJoke); // i can delete it 
-    app.get("/api/jokes/new", JokeController.createNewJoke);
-    app.get("/api/jokes/update/:_id", JokeController.updateSpecificJoke);
-    app.get("api/jokes/delete/:_id", JokeController.deleteSpecificJoke);
+    app.post("/api/jokes/new", JokeController.createNewJoke);
+    app.put("/api/jokes/update/:_id", JokeController.updateSpecificJoke);
+    app.delete("api/jokes/delete/:_id", JokeController.deleteSpecificJoke);
 }
